782c8bb2d15b7b09d9328270a81136b9d7fe853b
Bianca <biancancanal@gmail.com>
2021-11-14 19:38:19 -0600

    Add Successful GET Post with Item route Nested in User

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+ 
+  def create
+    item = Item.create.find(params[:id])
+    render json: review, include: :user
+  end
+  private
+
+  def record_not_found
+    render json: { error: "User not found"}, status: :not_found
   end
 
 end
